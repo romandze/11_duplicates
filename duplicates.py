20941548795805cdc1eec296b5859a830d710f90
Roman <ivastapas@gmail.com>
2018-03-16 17:38:25 +0300

    добавил кучу всяких функций

--- duplicates.py
+++ duplicates.py
@@ -11,9 +11,30 @@ def check_folder():
     else:
         print("Забыл указать директорию, попробуй запустить так: python duplicates.py ~/Video/")
 
+
+def get_file_size(filename):
+    filesize = os.path.getsize(filename)
+    return filesize
+
 def get_files_in_folder(directory):
-    pass
+    finded_files = []
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            path = os.path.join(root, filename)
+            finded_files.append({"path": path, "name": filename, "size": get_file_size(path)})
+    return finded_files
+
+def get_duplicates():
+    duplicates = []
+    all_files = get_files_in_folder(check_folder())
+    for i in range(len(all_files)-1):
+        for j in range(i+1, len(all_files)):
+            if (all_files[i]['name'] == all_files[j]['name']) and (all_files[i]['size'] == all_files[j]['size']):
+                duplicates.append(
+                    {'name': all_files[i]['name'], 'path': {all_files[i]['path'], all_files[j]['path']}})
+    return duplicates
 
 if __name__ == '__main__':
-    print(check_folder())
+
+    print(get_duplicates())
 
