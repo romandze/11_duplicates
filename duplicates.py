593112c10b74c38d21648bdc5876e279a47d6243
Roman <ivastapas@gmail.com>
2018-03-16 15:18:11 +0300

    add .gitignore and create function check_dir

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,19 @@
+import sys
+import os
+
+def check_folder():
+    if len(sys.argv) > 1:
+        if os.path.isdir(sys.argv[1]) == True:
+            dir = sys.argv[1]
+            return dir
+        else:
+            print("Нет такой директории")    
+    else:
+        print("Забыл указать директорию, попробуй запустить так: python duplicates.py ~/Video/")
+
+def get_files_in_folder(directory):
+    pass
 
 if __name__ == '__main__':
-    pass
+    print(check_folder())
+
